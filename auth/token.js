690d2c0b3166e039e971114d21fd91bd24d0c98c
Raissa Ferreira <rai200890@gmail.com>
2017-10-10 12:40:27 -0300

    Fix token generation

--- auth/token.js
+++ auth/token.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken"
 import config from "../config"
 
 export function generateToken(user) {
-    let payload = {"id": user.id}
+    let payload = {"sub": user.sub}
     return jwt.sign(payload, config.JWT_SECRET, {
         expiresIn: config.JWT_EXPIRATION_IN_MILISECONDS
     })
